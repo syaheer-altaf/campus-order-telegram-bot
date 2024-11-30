d4a96e0c0807227e8cfd05cc1811e2ee38a4a8dd
syaheer-altaf <altafsyaheer@gmail.com>
2024-11-30 22:21:32 +0800

    remove and update the unsafe method of storing API key

--- api.py
+++ api.py
@@ -1,1 +1,4 @@
-API_KEY = 'PASTE-YOUR-API-KEY-HERE'
+# set the environment variable in your system or deployment configuration:
+# export API_KEY='PASTE-YOUR-API-KEY-HERE'
+import os
+API_KEY = os.getenv('API_KEY')
